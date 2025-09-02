6221dbac551b7a9eb9ea3c1bf88c4e7975a82dd3
Osinachi Chibuzor <70204435+Chibuzor-byte@users.noreply.github.com>
2025-09-02 05:30:39 +0100

    Update app.js

--- Background-color Changer/App/app.js
+++ Background-color Changer/App/app.js
@@ -1,40 +1,30 @@
-*,
-*::before,
-*::after {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+const darkColorsArr = [
+  "#2C3E50",
+  "#34495E",
+  "#2C2C2C",
+  "#616A6B",
+  "#4A235A",
+  "#2F4F4F",
+  "#0E4B5A",
+  "#36454F",
+  "#2C3E50",
+  "#800020",
+];
+
+function getRandomIndex() {
+  const randomIndex = Math.floor(darkColorsArr.length * Math.random());
+  return randomIndex;
 }
 
-:root {
-  --yellow: #f1be32;
-  --golden-yellow: #feac32;
-  --dark-purple: #110815;
-  --light-grey: #efefef;
-}
+const body = document.querySelector("body");
+const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
 
-body {
-  background-color: var(--dark-purple);
-  color: var(--light-grey);
-  text-align: center;
-}
+function changeBackgroundColor() {
+  const color = darkColorsArr[getRandomIndex()];
 
-.bg-information-container {
-  margin: 15px 0 25px;
-  font-size: 1.2rem;
+  bgHexCodeSpanElement.innerText = color;
+  body.style.backgroundColor = color;
 }
+const btn = document.querySelector("#btn");
 
-.btn {
-  cursor: pointer;
-  padding: 10px;
-  margin: 10px;
-  color: var(--dark-purple);
-  background-color: var(--golden-yellow);
-  background-image: linear-gradient(#fecc4c, #ffac33);
-  border-color: var(--golden-yellow);
-  border-width: 3px;
-}
-
-.btn:hover {
-  background-image: linear-gradient(#ffcc4c, #f89808);
-}
+btn.onclick = changeBackgroundColor();
